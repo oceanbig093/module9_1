a2618c2c31b304bc10c70f2680e47c49cda71cd1
oceanbig093 <innervision41@mail.ru>
2024-11-05 16:10:35 +0300

    Update module9_1.py

--- module9_1.py
+++ module9_1.py
@@ -1,7 +1,7 @@
 def apply_all_func(int_list, *functions):
     results = {}
     for func in functions:
-        results[func] = func(int_list)
+        results[func.__name__] = func(int_list)
     return results
 
 print(apply_all_func([6, 20, 15, 9], max, min))
